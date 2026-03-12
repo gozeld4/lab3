94418656b3c487ace7b0b7243b7d7b891334f1d3
gozeld4 <150550981+gozeld4@users.noreply.github.com>
2026-03-12 14:25:46 -0400

    lab5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,25 @@
     import Project from '$lib/Project.svelte';
     import reading from '$lib/reading.json';
     import ReadingItem from '$lib/ReadingItem.svelte';
+    import { onMount } from "svelte";
+
+let githubData = null; // This will eventually hold our Github stats
+let loading = true; // This will be true *until* the fetch's promise resolves to a value
+let error = null; // If the API call resulted in an error, it will go into this variable
+
+
+
+let retrieveGithubData = async () => {
+    try {
+        let response = await fetch("https://api.github.com/users/gozeld4");
+        githubData = await response.json();
+    } catch (err) {
+        error = err;
+    }
+    loading = false;
+}
+
+    onMount(retrieveGithubData);
 </script>
 
 <h1>Gozel Dovranova</h1>
@@ -10,6 +29,28 @@
 
 <p>I am senior majoring in Chemical Engineering and AI. I am passionate about AI for Science.</p>
 <img src="images/1.jpg" alt="This is me">
+
+
+
+{#if loading}
+    <p>Loading...</p>
+{:else if error}
+    <p>Something went wrong: {error.message}</p>
+{:else}
+    <section>
+        <h2>My GitHub Stats</h2>
+        <dl>
+            <dt>Followers</dt>
+            <dd>{githubData.followers}</dd>
+            <dt>Following</dt>
+            <dd>{githubData.following}</dd>
+            <dt>Public Repositories</dt>
+            <dd>{githubData.public_repos}</dd>
+        </dl>
+    </section>
+{/if}
+
+
 
 <h2>Latest Projects</h2>
 <div class="projects highlights">
@@ -25,3 +66,30 @@
     {/each}
 </div>
 
+<style>
+dt {
+        grid-row: 2;
+        color: oklch(60% 5% 200);
+        text-transform: uppercase;
+        font-size: 0.75em;
+        letter-spacing: 0.05em;
+    }
+    dl {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        border: 1px solid oklch(50% 10% 200 / 40%);
+        border-radius: 0.5rem;
+        padding: 1rem;
+        gap: 0.5rem;
+        text-align: center;
+    }
+
+    
+
+    dd {
+        grid-row: 1;
+        font-size: 2.5em;
+
+        margin: 0;
+    }
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,20 +1,31 @@
 <script>
     import projects from '$lib/projects.json';
     import Project from '$lib/Project.svelte';
+    import ProjectNarrative from '$lib/ProjectNarrative.svelte';
+    let years = projects.map(proj => proj.year)
+    let range = Math.max(...years) - Math.min(...years);
 </script>
 
 <svelte:head>
   <title>Projects</title>
 </svelte:head>
 
-<h1>{ projects.length } Projects</h1>
+<h1>{projects.length} Projects over {range} Years</h1>
 
-    
-    <div class="projects">
-        {#each projects as project}
-        <Project data={project} />
-        {/each}
-    </div>
-    
+<p>Scroll down to see my a timeline of my projects and how they've contributed to my professional and personal life</p>
 
-    <p></p>
+<ProjectNarrative />
+
+<p class="outro">Thanks for scrolling through my project story! Feel free to explore all of the projects at your leisure below.</p>
+
+<div class="projects">
+    {#each projects as project}
+    <Project data={project} />
+    {/each}
+</div>
+
+<style>
+    .outro {
+        margin-bottom: 2rem;
+    }
+</style>
